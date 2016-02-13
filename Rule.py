b5fefdebfecfed409267c4d0e05802854a405160
Edward Anderson <nilbus@nilbus.com>
2016-02-13 10:35:44 -0500

    Improve Rule object pairing

--- Rule.py
+++ Rule.py
@@ -20,21 +20,27 @@ class Rule:
         Go through the smallest set of objects, and pair each with best matches from the
         other set. If the other set is larger, its worst-matching objects will remain
         unpaired.
-        @return a list of pair tuples, with None replacing any nonexistent object
+        @return a list of pair tuples, with a NoObject instance replacing any nonexistent object
         '''
         pairs = []
         smaller_set = set_a.values() if len(set_a) <= len(set_b) else set_b.values()
         larger_set  = set_a.values() if len(set_a) >  len(set_b) else set_b.values()
+        # FIXME: Pairing is not always happening right. The best matches in
+        # larger_set are being snatched up by earlier elements in smaller_set when
+        # there is no great match.
         for left_obj in smaller_set:
             similarities = np.array([self._obj_similarity(left_obj, right_obj) for right_obj in larger_set])
             most_similar_index = similarities.argmax()
             most_similar = larger_set.pop(most_similar_index)
             pairs.append((left_obj, most_similar))
         for right_obj in larger_set:
-            pairs.append((None, right_obj))
-        if smaller_set == set_b:
+            pairs.append((NoObject(), right_obj))
+        if smaller_set == set_b.values():
             reverse = lambda pair: tuple(reversed(pair))
             pairs = map(reverse, pairs)
+        # for pair in pairs:
+        #     print map(lambda object: object.attributes, pair)
+        # print
         return pairs
 
     def _obj_similarity(self, obj_a, obj_b):
